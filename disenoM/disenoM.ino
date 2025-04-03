2f64e8e597890ae4d0d3ded779377c533e1f6acc
Robotics Lab User <wnilbert@gmail.com>
2025-04-02 19:46:51 -0600

    3 POTENCIOMETRO

--- disenoM/disenoM.ino
+++ disenoM/disenoM.ino
@@ -1,81 +1,59 @@
-// CODIGO PARA MOTOR DE 2 TIEMPOS
+// Pines de los potenciómetros
+const int potPin1 = 34;
+const int potPin2 = 35;
+const int potPin3 = 32;
 
-//PINES PARA LOS POT LINEALES
-const int potPin1 = 34; 
-const int potPin2 = 34;
-const int potPin3 = 34;
-const int potPin4 = 34;
-const int potPin5 = 34;
+// Pines de los botones
+const int button1 = 21;
+const int button2 = 22;
+const int button3 = 23;
 
-const int button = 35; // FINAL DE CARRERA
-
-//LED'S RGB
-const int ledAzul1 = 4; //LED1
+// Pines de los LEDs RGB
+const int ledAzul1 = 4;
 const int ledRojo1 = 2;
-
-const int ledAzul2 = 16; //LED2
+const int ledAzul2 = 16;
 const int ledRojo2 = 17;
+const int ledAzul3 = 18;
+const int ledRojo3 = 19;
 
-const int motor = 23; // TALVES LA QUITEMOS
-
-
-//VARIABLES AUXILIARES
-unsigned long previousTime = 0;
-int potValor = 0;
-int ciclo = 0;
-int periodo = 0;
-
+// Configuración de PWM
 const int frecuencia = 500;
 const int resolucion = 8;
-const int segundo = 2000;
 
-void setup() { 
-  Serial.begin(9600); // INICIALIZA EL SERIAL
-  ledcAttach(motor, frecuencia, resolucion);
-  ledcAttach(ledAzul1, frecuencia, resolucion);  
-  ledcAttach(ledAzul2, frecuencia, resolucion);  
-  pinMode(button, INPUT);
+void setup() {
+  Serial.begin(9600);
+
+  // Asignación de PWM a LEDs
+  ledcAttach(ledAzul1, frecuencia, resolucion);
+  ledcAttach(ledAzul2, frecuencia, resolucion);
+  ledcAttach(ledAzul3, frecuencia, resolucion);
+ 
+
   pinMode(ledRojo1, OUTPUT);
   pinMode(ledRojo2, OUTPUT);
+  pinMode(ledRojo3, OUTPUT);
+
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
 }
 
 void loop() {
-  unsigned long currentTime = millis();
-  if((currentTime - previousTime) >= segundo){
-      previousTime = currentTime;
-      Serial.println(ciclo);
-      ciclo = map(ciclo, 0, 10, 55, 255);
-      ledcWrite(motor, ciclo);
-      ciclo = 0;
-  }
-
-  potValor = analogRead(potPin);
-  potValor = map(potValor, 0, 4095, 0, 255);
-  
-
-  if(digitalRead(button) == HIGH){
-    digitalWrite(ledRojo, HIGH);
-    ledcWrite(ledAzul, LOW);
-  }else{
-    ledcWrite(ledAzul, potValor);
-    digitalWrite(ledRojo, LOW);
-  }
-
-  potValor = map(potValor, 0, 255, 0, 100);
-  //Serial.println(potValor);
-  if(periodo == 0){
-    if((potValor >= 90) && (potValor <= 100))
-    {
-      periodo = 1;
-    }
-  }else
-  {
-    if((potValor >= 0) && (potValor <= 10))
-    {
-      periodo = 0;
-      ciclo++;
-    } 
-  }
-
+  controlarLed(potPin1, button1, ledAzul1, ledRojo1, 0);
+  controlarLed(potPin2, button2, ledAzul2, ledRojo2, 1);
+  controlarLed(potPin3, button3, ledAzul3, ledRojo3, 2);
   delay(100);
+}
+
+void controlarLed(int potPin, int buttonPin, int ledAzul, int ledRojo, int canalPWM) {
+  int potValor = analogRead(potPin);
+  potValor = map(potValor, 0, 4095, 0, 255);
+
+  if (digitalRead(buttonPin) == HIGH) {
+    digitalWrite(ledRojo, HIGH);
+    ledcWrite(ledAzul, 0);
+  } else {
+    digitalWrite(ledRojo, LOW);
+    ledcWrite(ledAzul, potValor);
+  }
 }
