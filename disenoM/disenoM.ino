0830b7614bc80c3f5231249eca63968acf083265
Robotics Lab User <wnilbert@gmail.com>
2025-04-06 14:38:16 -0600

    4 ELEMENTOS DE CADA UNO

--- disenoM/disenoM.ino
+++ disenoM/disenoM.ino
@@ -2,19 +2,23 @@
 const int potPin1 = 34;
 const int potPin2 = 35;
 const int potPin3 = 32;
+const int potPin4 = 33;
 
 // Pines de los botones
-const int button1 = 21;
-const int button2 = 22;
-const int button3 = 23;
+const int button1 = 14;
+const int button2 = 12;
+const int button3 = 13;
+const int button4 = 27;
 
 // Pines de los LEDs RGB
 const int ledAzul1 = 4;
 const int ledRojo1 = 2;
-const int ledAzul2 = 16;
-const int ledRojo2 = 17;
+const int ledAzul2 = 17;
+const int ledRojo2 = 16;
 const int ledAzul3 = 18;
-const int ledRojo3 = 19;
+const int ledRojo3 = 5;
+const int ledAzul4 = 23;
+const int ledRojo4 = 22;
 
 // Configuración de PWM
 const int frecuencia = 500;
@@ -27,21 +31,25 @@ void setup() {
   ledcAttach(ledAzul1, frecuencia, resolucion);
   ledcAttach(ledAzul2, frecuencia, resolucion);
   ledcAttach(ledAzul3, frecuencia, resolucion);
+  ledcAttach(ledAzul4, frecuencia, resolucion);
  
 
   pinMode(ledRojo1, OUTPUT);
   pinMode(ledRojo2, OUTPUT);
   pinMode(ledRojo3, OUTPUT);
+  pinMode(ledRojo4, OUTPUT);
 
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
   pinMode(button3, INPUT);
+  pinMode(button4, INPUT);
 }
 
 void loop() {
   controlarLed(potPin1, button1, ledAzul1, ledRojo1, 0);
   controlarLed(potPin2, button2, ledAzul2, ledRojo2, 1);
   controlarLed(potPin3, button3, ledAzul3, ledRojo3, 2);
+  controlarLed(potPin4, button4, ledAzul4, ledRojo4, 2);
   delay(100);
 }
 
